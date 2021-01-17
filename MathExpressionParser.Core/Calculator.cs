146e0b97203c2da4d74e6930f50b1576a7c7f6b7
zaparivannyi <zaparivannyi@gmail.com>
2021-01-17 14:33:10 +0300

    Herbert Schildt realization

--- MathExpressionParser.Core/Calculator.cs
+++ MathExpressionParser.Core/Calculator.cs
@@ -1,13 +1,20 @@
 ﻿using MathExpression.Core;
 using System;
+using TParser = MathExpressionParser.Core.Parser.Parser;
 
 namespace MathExpressionParser.Core
 {
     public class Calculator : ICalculator
     {
+        private TParser _parser;
+        public Calculator()
+        {
+            _parser = new TParser();
+        }
+
         public double CalculateExpression(string expression)
         {
-            throw new NotImplementedException();
+            return _parser.Evaluate(expression);
         }
     }
 }
